8d296f273dde9aec969a8361dd487ca58f6a22b1
Quentin Laffont <qlaffont13@gmail.com>
2020-01-22 23:07:14 +0100

    fix(authservice): fix issues + Add FastifyInstanceAuth

--- server/services/auth/authService.ts
+++ server/services/auth/authService.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { forbidden, unauthorized } from '@hapi/boom';
 import jwt from 'jsonwebtoken';
 import { FastifyInstance } from 'fastify';
@@ -5,43 +6,44 @@ import { writeFileSync } from 'fs';
 
 import { HandlingBoom } from '../httpHandling/httpHandlingService';
 
-export const generateToken = (): void => {
+export interface FastifyInstanceAuth extends FastifyInstance {
+  authToken?: string | boolean;
+}
+
+export const generateToken = (): string => {
   const secret = process.env.AUTH_SECRET || 'monity-secret';
-  const token = jwt.sign({ foo: 'bar' }, secret, { expiresIn: '1d' });
+  const token = jwt.sign({ pkg: 'monity' }, secret, { expiresIn: '1d' });
   return token;
 };
 
 export const saveTokenInFile = (token): void => {
-  return writeFileSync(__dirname + 'auth-token.txt', token);
+  return writeFileSync(process.cwd() + '/auth-token.txt', token);
 };
 
-export const generateAndSaveToken = (fastify: FastifyInstance): void => {
+export const generateAndSaveToken = (fastify: FastifyInstanceAuth): string => {
   const token = generateToken();
 
-  //@ts-ignore
   fastify.authToken = token;
 
   return token;
 };
 
-export const checkTokenValidity = (token: string): string | Error => {
-  return jwt.verify(token, process.env.AUTH_SECRET);
+export const checkTokenValidity = (token: string): any | Error => {
+  return jwt.verify(token, process.env.AUTH_SECRET || 'monity-secret');
 };
 
-export const verifyAuth = (fastify: FastifyInstance) => (request, reply, done): void => {
+export const verifyAuth = (fastify: FastifyInstanceAuth) => (request, reply, done): void => {
   let token;
 
   if (request.headers.authorization || request.headers.Authorization) {
     token = request.headers.authorization || request.headers.Authorization;
   }
 
-  // @ts-ignore
   if (token && fastify.authToken && token === fastify.authToken) {
     try {
       checkTokenValidity(token);
       done();
     } catch (error) {
-      // @ts-ignore
       fastify.authToken = false;
       HandlingBoom(forbidden('Token expired !'), reply);
       done();
