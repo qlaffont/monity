9ba252c9a792a16e31bf3fcf27b49cd29eaa3391
Quentin Laffont <qlaffont13@gmail.com>
2020-01-28 15:31:05 +0100

    test(authroutes): add test with 403 return

--- server/components/auth/authRoutes.test.ts
+++ server/components/auth/authRoutes.test.ts
@@ -3,15 +3,15 @@ import app from '../../server';
 describe('AuthRoutes', () => {
   let fastify;
 
-  beforeAll(done => {
-    fastify = app();
-
-    fastify.ready(() => {
-      done();
-    });
-  });
-
   describe('GET /auth/generate', () => {
+    beforeEach(done => {
+      fastify = app();
+
+      fastify.ready(() => {
+        done();
+      });
+    });
+
     it('should create a token', async () => {
       const res = await fastify.inject({
         method: 'GET',
@@ -19,6 +19,19 @@ describe('AuthRoutes', () => {
       });
       expect(res.statusCode).toEqual(200);
       expect(JSON.parse(res.body)).toMatchObject({ message: 'Token successfully created !' });
+    });
+
+    it('should send 403 if we try to generate token before it expired', async () => {
+      await fastify.inject({
+        method: 'GET',
+        url: '/auth/generate',
+      });
+      const res = await fastify.inject({
+        method: 'GET',
+        url: '/auth/generate',
+      });
+      expect(res.statusCode).toEqual(403);
+      expect(JSON.parse(res.body)).toMatchObject({ message: 'Token already exist !' });
     });
   });
 });
