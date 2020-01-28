f379eeb90d1d3fe4e50978af10a8d9fe97870852
Quentin Laffont <qlaffont13@gmail.com>
2020-01-28 16:05:33 +0100

    improvement(auth): restrict /oauth/generate to localhost

--- server/components/auth/authRoutes.ts
+++ server/components/auth/authRoutes.ts
@@ -1,6 +1,18 @@
 import { AuthController } from './authController';
 import { generateToken } from './authSchema';
+import { HandlingBoom } from '../../services/httpHandling/httpHandlingService';
+import { forbidden } from '@hapi/boom';
+
+const localhostCheck = (req, res, done): void => {
+  if (req.ip === '127.0.0.1') {
+    done();
+  } else {
+    HandlingBoom(forbidden(), res);
+  }
+};
 
 export default (app): void => {
-  app.get('/auth/generate', { schema: generateToken }, (req, res) => AuthController.generateToken(req, res, app));
+  app.get('/auth/generate', { schema: generateToken, onRequest: localhostCheck }, (req, res) =>
+    AuthController.generateToken(req, res, app),
+  );
 };
