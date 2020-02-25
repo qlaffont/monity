26d8574aa7779a3caa05bd6877085e10bad75155
Quentin Laffont <qlaffont13@gmail.com>
2020-02-25 13:46:12 +0100

    debug(travis): add debug to travis error

--- server/components/metrics/metricsController.ts
+++ server/components/metrics/metricsController.ts
@@ -50,6 +50,7 @@ export class MetricsController {
       const result = await MetricsService.prometheusExport();
       return res.status(200).send(result);
     } catch (error) {
+      console.log(error);
       return res.code(500).send('monity_up 0');
     }
   }
