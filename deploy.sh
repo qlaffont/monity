16c2a451ae08950e6b2670239cbea953bd7566e9
Quentin Laffont <qlaffont13@gmail.com>
2020-02-26 12:40:15 +0100

    ci(travis): push Latest image to docker

--- deploy.sh
+++ deploy.sh
@@ -9,3 +9,4 @@ docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${IMAGE}:${GIT_VERSION}
+docker push ${IMAGE}:latest
