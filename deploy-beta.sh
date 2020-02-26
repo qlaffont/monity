546fd3484c65d0bfdd568f4aea8a68e061ff552a
Quentin Laffont <qlaffont13@gmail.com>
2020-02-26 08:32:57 +0100

    ci(docker): fix build

--- deploy-beta.sh
+++ deploy-beta.sh
@@ -4,7 +4,6 @@ set -e
 IMAGE="qlaffont/monity"
 
 docker build -t ${IMAGE}:beta .
-docker tag ${IMAGE}:beta
 
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${IMAGE}:beta
