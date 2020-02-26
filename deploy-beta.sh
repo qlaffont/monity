53c1fb3f251842256a557c80b94b13c80464e6fb
Quentin Laffont <qlaffont13@gmail.com>
2020-02-26 08:24:55 +0100

    ci(travis): add docker build on develop branch

---
+++ deploy-beta.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+IMAGE="qlaffont/monity"
+
+docker build -t ${IMAGE}:beta
+docker tag ${IMAGE}:beta
+
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker push ${IMAGE}:beta
